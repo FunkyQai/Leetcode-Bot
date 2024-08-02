686568d49e27eecdca246ae0570d95382bd2561c
QaiGear5 <abdulqaiyumlee@gmail.com>
2024-08-03 03:42:39 +0800

    added gitignore, readme and requirements.txt

---
+++ config.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+TOKEN: Final = "7365742466:AAGPVY32H69xIxC4GQfUefRk9nb9vjCb0U0"
+BOT_USERNAME: Final = "@LeEtC0de_bot"
+USERS = ["qaiyumlee", "shafiqninaba"]
---
+++ main.py
@@ -0,0 +1,104 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import requests
+from datetime import datetime
+import pytz
+from bs4 import BeautifulSoup
+from config import TOKEN, BOT_USERNAME, USERS
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Reminder to practice LeetCode daily. Use /QOD to get the Question of the Day and /Submissions to get the daily submissions.")
+
+async def question_of_the_day(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = fetch_qod()
+    formatted_data = format_qod(data)
+    await update.message.reply_text(formatted_data, disable_web_page_preview=True)
+
+async def daily_submissions(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    combined_text = ""
+    for user in USERS:
+        submissions = get_user_submissions(user)
+        todays_submissions = get_todays_submissions(submissions)
+        if todays_submissions:
+            combined_text += f"User: {user}\n"
+            for subs in todays_submissions:
+                combined_text += f"Title: [{subs['title']}]({subs['url']})\nCompleted on: {subs['timestamp']}\n\n"
+        else:
+            combined_text += f"User: {user}\nNo submissions for today.\n\n"
+
+    await update.message.reply_text(combined_text if combined_text else "No submissions for today.", disable_web_page_preview=True)
+
+
+# Error Logging
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+# Helper Functions
+def fetch_qod():
+    url = "https://alfa-leetcode-api.onrender.com/daily"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
+def format_qod(data):
+    title = data.get("questionTitle", "N/A")
+    difficulty = data.get("difficulty", "N/A")
+    question = parse_html(data.get("question", "N/A"))
+    url = data.get("questionLink", "N/A")
+
+    formatted_string = (f"Title:   [{title}]({url})\n"
+                        f"Difficulty:   {difficulty}\n\n"
+                        f"Question:\n{question}\n")
+    return formatted_string
+
+def parse_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.get_text()
+
+def get_user_submissions(username):
+    url = f"https://alfa-leetcode-api.onrender.com/{username}/acSubmission?limit=10"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("submission", [])
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+
+def get_todays_submissions(submissions):
+    sg_timezone = pytz.timezone("Asia/Singapore")
+    today = datetime.now(sg_timezone).date()
+    base_url = "https://leetcode.com/problems/"
+    todays_submissions = [
+        {
+            'title': sub.get('title'),
+            'url': f"{base_url}{sub.get('titleSlug')}/",
+            'timestamp': datetime.fromtimestamp(int(sub['timestamp']), sg_timezone).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        for sub in submissions
+        if datetime.fromtimestamp(int(sub['timestamp']), sg_timezone).date() == today
+    ]
+    return todays_submissions
+
+# Main
+if __name__ == "__main__":
+    print('Starting Bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler("Start", start_command))
+    app.add_handler(CommandHandler("QOD", question_of_the_day))
+    app.add_handler(CommandHandler("Submissions", daily_submissions))
+
+    # Error Logging
+    app.add_error_handler(error)
+
+    # Polling
+    print('Bot is running...')
+    app.run_polling(poll_interval=2)
+
+
+
+
